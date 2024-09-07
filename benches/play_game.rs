799d7560e71dbb17bd5918581796f94155ce33dd
Fedor Sakharov <fedor.sakharov@gmail.com>
2024-09-07 11:37:59 +0200

    flames (#3)

--- benches/play_game.rs
+++ benches/play_game.rs
@@ -1,10 +1,20 @@
+use iai_callgrind::{
+    library_benchmark, library_benchmark_group, main, FlamegraphConfig, LibraryBenchmarkConfig,
+};
 use iai_test::play_game;
+use std::hint::black_box;
 
+#[library_benchmark]
 fn bench_play_game() {
     for i in 1..=100 {
         play_game(i, false)
     }
-    iai::black_box(());
+    black_box(());
 }
 
-iai::main!(bench_play_game);
+library_benchmark_group!(name = play_game_group; benchmarks = bench_play_game);
+
+main!(
+    config = LibraryBenchmarkConfig::default().flamegraph(FlamegraphConfig::default());
+    library_benchmark_groups = play_game_group
+);
